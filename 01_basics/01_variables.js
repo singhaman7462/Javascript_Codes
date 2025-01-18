9da302576198735e550c0a9643d76718bd14e27d
singhaman7462 <138794681+singhaman7462@users.noreply.github.com>
2025-01-18 18:26:31 +0000

    Variables and their declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=1234;
+// console.log(accountId);
+let accountName="Aman Singh";
+// console.log(accountName);
+var accountEmail="Aman@gmail.com";
+// console.log(accountEmail);
+accountNum=123867;
+console.table([accountEmail,accountId,accountName,accountNum]);
