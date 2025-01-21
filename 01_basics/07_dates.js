e532185d38ace201298de96cf80b13ec11f31f39
singhaman7462 <138794681+singhaman7462@users.noreply.github.com>
2025-01-21 11:34:24 +0000

    Added comments

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -73,7 +73,7 @@ console.log(myNewDate.toLocaleString('default',{
 
 // Dates
 
-let myDate = new Date()
+// let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
@@ -82,26 +82,26 @@ let myDate = new Date()
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate = new Date("01-14-2023")
+// // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+// let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// // console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+// // console.log(Math.floor(Date.now()/1000));
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
-// `${newDate.getDay()} and the time `
+// // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
+// newDate.toLocaleString('default', {
+//     weekday: "long",
     
-})
+// })
 
 
 
