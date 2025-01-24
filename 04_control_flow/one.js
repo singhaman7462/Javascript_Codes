699f72c2c4ba1f9cd17e560f178fc87f825fb811
singhaman7462 <138794681+singhaman7462@users.noreply.github.com>
2025-01-24 11:59:22 +0000

    control flow codes iniated

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+//Control flow
+
+//If case
+if(2=="2"){
+    console.log("executed");
+}
+//No type checking in the above condition so true is returned
+
+if(2==="2"){
+    console.log(" again executed");
+}
+//Type checking is done so no output.
